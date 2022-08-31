95f976873e9e3468fa736b02b1c5ddc256f04793
Aymenob <aymenobba@gmail.com>
2022-09-01 01:55:00 +0200

    contact-list-backend-validation

--- validation.js
+++ validation.js
@@ -1,6 +1,4 @@
 const { check, validationResult } = require('express-validator');
-
-
 exports.validate=[check("Email").isEmail().withMessage("Missing an a valid Email!")
 , check("Firstname").notEmpty().withMessage("Missing a Valid First name!")
 ,check("Lastname").notEmpty().withMessage("Missing a Valid Last name!"),
